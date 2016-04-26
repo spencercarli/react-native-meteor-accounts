205dd2c9e15f7ae139844371f0fcbd7775193738
Spencer Carli <spencercarli@gmail.com>
2016-04-26 16:40:05 -0400

    Setup sign in form

--- RNPasswordExample/app/SignIn.js
+++ RNPasswordExample/app/SignIn.js
@@ -1,15 +1,44 @@
-import React, { StyleSheet, Text, View } from 'react-native';
+import React, { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions } from 'react-native';
+const { width } = Dimensions.get('window');
 
 class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Sign In</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(email) => this.setState({email})}
+          placeholder="Email"
+        />
+
+        <TextInput
+          style={styles.input}
+          onChangeText={(password) => this.setState({password})}
+          placeholder="Password"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Create Account</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+const ELEMENT_WIDTH = width - 40;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,6 +46,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  input: {
+    width: ELEMENT_WIDTH,
+    fontSize: 16,
+    height: 36,
+    padding: 10,
+    backgroundColor: '#FFFFFF',
+    borderColor: '#888888',
+    borderWidth: 1,
+    marginHorizontal: 20,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#3B5998',
+    width: ELEMENT_WIDTH,
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: '500',
+    fontSize: 16,
+  }
 });
 
 export default SignIn;
