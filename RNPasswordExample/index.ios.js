55820e62da4e02c1143d9b564812856f14fdad17
Spencer Carli <spencercarli@gmail.com>
2016-04-26 16:30:41 -0400

    Fix naming issue

--- RNPasswordExample/index.ios.js
+++ RNPasswordExample/index.ios.js
@@ -1,4 +1,4 @@
 import React, { AppRegistry } from 'react-native';
 import App from './app';
 
-AppRegistry.registerComponent('RNFacebookExample', () => App);
+AppRegistry.registerComponent('RNPasswordExample', () => App);
