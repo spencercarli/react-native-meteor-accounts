2f0031d68a85507a62f11e7fcd829ef9d5b2f677
Spencer Carli <spencercarli@gmail.com>
2016-04-26 19:11:41 -0400

    Ability to sign out

--- RNPasswordExample/app/SignOut.js
+++ RNPasswordExample/app/SignOut.js
@@ -1,21 +1,38 @@
-import React, { StyleSheet, Text, View } from 'react-native';
+import React, { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import Meteor from 'react-native-meteor';
+const { width } = Dimensions.get('window');
 
 class SignOut extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Sign Out</Text>
+        <TouchableOpacity style={styles.button} onPress={() => Meteor.logout()}>
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+const ELEMENT_WIDTH = width - 40;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  button: {
+    backgroundColor: '#3B5998',
+    width: ELEMENT_WIDTH,
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: '500',
+    fontSize: 16,
   },
 });
 
